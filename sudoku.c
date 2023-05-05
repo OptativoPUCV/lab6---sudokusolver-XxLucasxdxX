db1ec60138d43a9934eb2d8e3a1cf393e9d9e03c
Replit user <>
2023-05-05 19:14:14 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -115,19 +115,21 @@ List* get_adj_nodes(Node* n){
           if (is_valid(nodo)){
             pushBack(list, nodo);
           } 
-          
         }
       return list;
       }
-    }
-    
+    }  
   }
   return list;
 }
 
 
 int is_final(Node* n){
-    return 0;
+  for(int i = 0 ; i < 9 ; i++)
+    for(int j = 0 ; j < 9 ; j++)
+      if(n->sudo[i][j]==0)
+        return 0;
+  return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
